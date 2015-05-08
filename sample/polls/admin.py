945335b9fcf8089fd9d5dd067cfe42e35278c75b
Meet Vora <v.h.c.meetirth@gmail.com>
2015-05-08 22:16:25 +0530

    Tutorial 4 completed. 100% Working

--- sample/polls/admin.py
+++ sample/polls/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from .models import Question, Choice
 list_filter = ['pdate']
 admin.site.register(Question)
+admin.site.register(Choice)
 # Register your models here.
--- sample/polls/views.py
+++ sample/polls/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import Http404
-from django.http import HttpResponse
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.core.urlresolvers import reverse
 def index(request):
 	latest_question_list = Question.objects.order_by('-pdate')[:3]
     	output = {'latest_question_list':latest_question_list}
@@ -13,6 +13,17 @@ def detail(request, question_id):
 		raise Http404("Question doesn't exist")
 	return render(request, 'polls/detail.html', {'question':q})
 def results(request, question_id):
-	return ("Currently at results of Question %s." %question_id)
+	q= get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/result.html', {'question':q})
 def vote(request, question_id):
-	return HttpResponse("Voting for Question %s."%question_id)
+	e = get_object_or_404(Question, pk=question_id)
+	try:
+		sel_choice = e.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		p ={'question': e, 'error_message': "Are you sure you made a decision?"}
+		return render (request, 'polls/detail.html', p)
+	else:
+		sel_choice.votes+=1
+		sel_choice.save()
+
+	return HttpResponseRedirect(reverse('polls:result', args=(int(e.id),)))
