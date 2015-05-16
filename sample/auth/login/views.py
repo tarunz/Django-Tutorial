1a7156a5711ebd1d570a29de35926ee2e57089a3
Meet Vora <v.h.c.meetirth@gmail.com>
2015-05-16 23:29:22 +0530

    Everything working perfect, except web field in Models.

--- sample/auth/login/views.py
+++ sample/auth/login/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import UserProfile 
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 logged = False
 username = ""
@@ -40,28 +41,29 @@ def signin(request):
 			else:
 				return HttpResponse("Your account isn't activated yet :(")
 		else:
-			HttpResponse("Incorrect credentials \n")
+			return HttpResponse("Incorrect credentials \n")
 	else:
 		return render_to_response('login/signin.html', {}, context)
 
+@login_required(redirect_field_name='/login/signin')
 def renew(request):
     global username
+    #import pdb; pdb.set_trace() ;
     if request.method=='POST':
 	   if request.POST['newpass1']== request.POST['newpass2']:
             nmail= request.POST['newmail']
             passw= request.POST['oldpass']
             passn= request.POST['newpass1']
             nweb = request.POST['newweb']
-            if passn!=None:
-                u = User.objects.get(username='username')
-                u.set_password(passn)
-                u.save()
-            if nmail!=None:
+            try:
                 u = User.objects.get(username=username)
-                u.email = nmail
+                if passn!="":
+                    u.set_password(passn)
+                if nmail!="":
+                    u.email = nmail
+                if nweb!="":
+                    u.web = nweb;
                 u.save()
-            if nweb!=None:
-                u = User.objects.get(username=username)
-                u.web = nweb;
-                u.save()
+            except username.DoesNotExist:
+                return None
     return HttpResponseRedirect('/login/')
